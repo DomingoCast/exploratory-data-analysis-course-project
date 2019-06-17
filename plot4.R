7bb09ae68091df59bc5117e519d59ebba8098a1f
sortzgandea <48081621+sortzgandea@users.noreply.github.com>
2019-06-17 13:26:46 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,9 @@
+direc <- "C:/Users/aquad/Desktop/Plan/Data/Exploratory analisis/exdata_data_NEI_data"
+setwd(direc)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
 merged <- merge(NEI, SCC, by = "SCC")
 Coal <- grep("Coal", merged$EI.Sector)
 sumCoal <- tapply(merged$Emissions[Coal],as.factor(merged$year[Coal]), sum)
